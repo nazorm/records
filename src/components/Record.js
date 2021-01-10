6712bd9636f0fc3fb57f09314406d085c8ea9c69
Nazorm <nnadoziechinazor@gmail.com>
2021-01-10 19:26:24 +0100

    updated record component

--- src/components/Record.js
+++ src/components/Record.js
@@ -2,18 +2,19 @@ import React from 'react';
 import '../App.css'
 const Redcord = (props) => {
 	return (
-		<div>
-			<h3>
+		<a href= {props.url} target='_blank' rel='noreferrer' className='record'>
+			<h3 className='patient-name'>
 				{props.firstname} {props.lastname}
 			</h3>
 			<div className="details">
-				<span>Username: {props.username}</span>
-				<span>Gender: {props.gender} </span>
-                <span>Phone Number: {props.phonenumber}</span>
-				<span>Payment Method: {props.paymentnethod}</span>
-				<span>Last Login: {props.lastlogin}</span>
+				<span className='detail-parent'>Username: <span className='detail'>{props.username} </span></span>
+				<span className='detail-parent'>Gender: <span className='detail'>{props.gender} </span></span>
+                <span className='detail-parent'>Phone Number: <span className='detail'>{props.phonenumber} </span></span>
+				<span className='detail-parent'>Credit Card Type: <span className='detail'>{props.creditcard} </span></span>
+				<span className='detail-parent'>Payment Method:  <span className='detail'>{props.paymentmethod}</span></span>
+				<span className='detail-parent'>Last Login: <span className='detail'>{props.lastlogin} </span></span>
 			</div>
-		</div>
+		</a>
 	);
 };
 
