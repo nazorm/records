d2c359f53337071ab5b89dcc65233a1951195ac8
Nazorm <nnadoziechinazor@gmail.com>
2021-01-10 19:25:27 +0100

    worked on filters

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import './App.css';
 import Pagination from './components/Pagination';
 import Recordspage from './components/Recordspage';
+import Filter from './components/Filter';
 function App() {
 	const [patientRecordList, setPatientRecordList] = useState([]);
 	const [loading, setLoading] = useState(false);
@@ -16,22 +17,62 @@ const fetchRecords = async()=>{
   setPatientRecordList(res.data.records.profiles)
   setLoading(false);
 }
+
 fetchRecords()
   },[]);
+
+
+  
   //get current posts
   const indexofLastPost = currentPage*postsPerPage;
   const indexofFirstPost = indexofLastPost- postsPerPage;
   const currentPosts = patientRecordList.slice(indexofFirstPost, indexofLastPost)
+
   //paginate
   const handlePage=(number)=>{
 	setCurrentPage(number)
 
   }
- 
- 
+  //handle Resst
+	const fetchResetData = async()=>{
+		setLoading(true);
+		const res = await axios.get('http://api.enye.tech/v1/challenge/records');
+		setPatientRecordList(res.data.records.profiles)
+		setLoading(false);
+  }
+  //handle gender filter
+ const handleGenderChange=(e)=>{
+	 const {value} = e.target;
+	 const posts = patientRecordList.filter((d)=>{
+		 return d.Gender === value
+	 })
+	 setPatientRecordList(posts)
+	
+ }
+ //handle payment type filter
+ const handlePaymentChange=(e)=>{
+const {value} = e.target
+const posts = patientRecordList.filter((d)=>{
+	return d.PaymentMethod === value
+})
+setPatientRecordList(posts)
+ }
+ //handle credit card filter
+
   return (
 		<div className="App">
-			<h1>Patient Records</h1>
+			<div className='header'>
+			<h1 className='file-name'>Patient Records</h1>
+			<div className='filter-container'>
+			<Filter
+			handleGenderChange={handleGenderChange}
+			handlePaymentChange={handlePaymentChange}
+
+			/>
+			<button className='reset-btn' onClick={()=>fetchResetData()}>Reset</button>
+			</div> 
+			</div>
+			
 			{loading ? <p>Loading...</p> : <Recordspage patientrecords={currentPosts} />}
 			<Pagination
 			 postsPerPage={postsPerPage} 
