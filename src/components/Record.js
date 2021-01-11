71db31c3e0a82e0d593dbf7694153cf5a27d9b57
Nazorm <nnadoziechinazor@gmail.com>
2021-01-11 22:54:58 +0100

    semantic tags

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import '../App.css'
+import '../App.css';
 const Redcord = (props) => {
 	return (
-		<a href= {props.url} target='_blank' rel='noreferrer' className='record'>
-			<h3 className='patient-name'>
+		<a href={props.url} target="_blank" rel="noreferrer" className="record">
+			<h3 className="patient-name">
 				{props.firstname} {props.lastname}
 			</h3>
 			<div className="details">
-				<span className='detail-parent'>Username: <span className='detail'>{props.username} </span></span>
-				<span className='detail-parent'>Gender: <span className='detail'>{props.gender} </span></span>
-                <span className='detail-parent'>Phone Number: <span className='detail'>{props.phonenumber} </span></span>
-				<span className='detail-parent'>Credit Card Type: <span className='detail'>{props.creditcard} </span></span>
-				<span className='detail-parent'>Payment Method:  <span className='detail'>{props.paymentmethod}</span></span>
-				<span className='detail-parent'>Last Login: <span className='detail'>{props.lastlogin} </span></span>
+				<p className="detail-parent">
+					Username: <span className="detail">{props.username} </span>
+				</p>
+				<p className="detail-parent">
+					Gender: <span className="detail">{props.gender} </span>
+				</p>
+				<p className="detail-parent">
+					Phone Number: <span className="detail">{props.phonenumber} </span>
+				</p>
+				<p className="detail-parent">
+					Credit Card Type: <span className="detail">{props.creditcard} </span>
+				</p>
+				<p className="detail-parent">
+					Payment Method: <span className="detail">{props.paymentmethod}</span>
+				</p>
+				<p className="detail-parent">
+					Last Login: <span className="detail">{props.lastlogin} </span>
+				</p>
 			</div>
 		</a>
 	);
