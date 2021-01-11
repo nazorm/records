5c20c17f8f615c430c040d3e888073bd70f58bb2
Nazorm <nnadoziechinazor@gmail.com>
2021-01-11 22:53:39 +0100

    set user select options

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,48 +1,26 @@
+import React from 'react';
+import { Select } from 'antd';
+const { Option } = Select;
+const Filter = (props) => {
+	return (
+		<div className="select-container">
+			<label className="label">
+				<Select defaultValue="--Gender--" style={{ width: 130 }} onChange={props.handleGenderChange}>
+					<Option value="Female">Female</Option>
+					<Option value="Male">Male</Option>
+					<Option value="Prefer to skip">Prefer to Skip</Option>
+				</Select>
+			</label>
+			<label className="label">
+				<Select defaultValue="--Payment Method--" style={{ width: 150 }} onChange={props.handlePaymentChange}>
+					<Option value="paypal"> PayPal</Option>
+					<Option value="check">Cheque</Option>
+					<Option value="money order">Money Order</Option>
+					<Option value="cc">CC</Option>
+				</Select>
+			</label>
+		</div>
+	);
+};
 
-import React from 'react'
-
-const Filter =(props)=>{
-return(
-<label >
-				Filter by: 
-				<select onChange={props.handleGenderChange} className='gender'>
-				    <option>--Gender--</option>
-					<option
-					name = 'Gender'
-					value ='Female'
-					>Female</option>
-				<option
-					name = 'Gender'
-					value ='Male'
-					>Male</option>
-					<option
-					name = 'Gender'
-					value ='Prefer to skip'
-					>Prefer to Skip</option>
-				</select>
-
-				<select onChange={props.handlePaymentChange} >
-				    <option>--Payment Method--</option>
-					<option
-					name = 'PaymentMethod'
-					value ='paypal'
-					>PayPal</option>
-				<option
-					name = 'PaymentMethod'
-					value ='check'
-					>Cheque</option>
-					<option
-					name = 'PaymentMethod'
-					value ='money order'
-					>Money Order</option>
-					<option
-					name = 'PaymentMethod'
-					value ='cc'
-					>CC</option>
-				</select>
-			</label> 
-  
-  )
-}
-
-export default Filter
+export default Filter;
