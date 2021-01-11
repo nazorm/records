bf4aede9d0ece9e23abf5ce15a167c8d201a7e83
Nazorm <nnadoziechinazor@gmail.com>
2021-01-11 22:53:56 +0100

    pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import '../App.css'
-const Pagination =({postsPerPage, totalPosts, handlePage})=>{
-const pageNumbers =[];
+import '../App.css';
+const Pagination = ({ postsPerPage, totalPosts, handlePage }) => {
+	const pageNumbers = [];
 
-for (let i=1; i<=Math.ceil(totalPosts/postsPerPage); i++){
-    pageNumbers.push(i)
-}
+	for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+		pageNumbers.push(i);
+	}
 
-
-
-    return(
-        <div className='page-number'>
-             {pageNumbers.map(number=>(
-                 <button className='page-number-button' key={number} onClick={()=>handlePage(number)}>
-                     {number}
-                     </button>
-             ))}
-        </div>
-    )
-}
-
+	return (
+		<div className="page-number">
+			{pageNumbers.map((number) => (
+				<button className="page-number-button" key={number} onClick={() => handlePage(number)}>
+					{number}
+				</button>
+			))}
+		</div>
+	);
+};
 
 export default Pagination;
