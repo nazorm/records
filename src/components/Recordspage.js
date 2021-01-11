e643341426e877676aed765c33253fff4870d4f6
Nazorm <nnadoziechinazor@gmail.com>
2021-01-11 22:55:27 +0100

    record

--- src/components/Recordspage.js
+++ src/components/Recordspage.js
@@ -1,31 +1,26 @@
 import React from 'react';
-import Record from './Record'
-import '../App.css'
+import Record from './Record';
+import '../App.css';
 
-const Recordspage = ({patientrecords}) => {
+const Recordspage = ({ patientrecords }) => {
+	const records = patientrecords.map((d, index) => {
+		return (
+			<Record
+				key={index}
+				url={d.URL}
+				firstname={d.FirstName}
+				lastname={d.LastName}
+				username={d.UserName}
+				gender={d.Gender}
+				creditcard={d.CreditCardType}
+				phonenumber={d.PhoneNumber}
+				paymentmethod={d.PaymentMethod}
+				lastlogin={d.LastLogin}
+			/>
+		);
+	});
 
-    const records = patientrecords.map((d, index)=>{
-return(
-    <Record
-    key ={index}
-    url = {d.URL}
-    firstname ={d.FirstName}
-    lastname ={d.LastName}
-    username = {d.UserName}
-    gender = {d.Gender}
-    creditcard = {d.CreditCardType}
-    phonenumber ={d.PhoneNumber}
-    paymentmethod={d.PaymentMethod}
-    lastlogin = {d.LastLogin}
-    />
-)
-    })
-
-	return (
-		<div>
-            {records}
-		</div>
-	)
+	return <div className="record-container">{records}</div>;
 };
 
 export default Recordspage;
