e3a74dbde1fcfed9182aaf40b017f7bb4a76acbd
Nazorm <nnadoziechinazor@gmail.com>
2021-01-13 14:08:12 +0100

    set select

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -5,14 +5,14 @@ const Filter = (props) => {
 	return (
 		<div className="select-container">
 			<label className="label">
-				<Select defaultValue="--Gender--" style={{ width: 130 }} onChange={props.handleGenderChange}>
+				<Select defaultValue="--Gender--" style={{ width: 130 }} onChange={props.handleSelectChange}>
 					<Option value="Female">Female</Option>
 					<Option value="Male">Male</Option>
 					<Option value="Prefer to skip">Prefer to Skip</Option>
 				</Select>
 			</label>
 			<label className="label">
-				<Select defaultValue="--Payment Method--" style={{ width: 150 }} onChange={props.handlePaymentChange}>
+				<Select defaultValue="--Payment Method--" style={{ width: 150 }} onChange={props.handleSelectChange}>
 					<Option value="paypal"> PayPal</Option>
 					<Option value="check">Cheque</Option>
 					<Option value="money order">Money Order</Option>
